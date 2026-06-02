f58c822137dd9fd76021c9e69ffb6d1c7a85bc53
Zero4nik <freefaer0tt@gmail.com>
2026-06-02 18:18:13 +0500

    Enable CORS for Vercel domain

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['https://realty-bot-prod-bjmn.vercel.app', 'https://t.me'],
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
