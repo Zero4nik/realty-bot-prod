a540084f6420da2a158baad1cfeda1dec1015644
Zero4nik <freefaer0tt@gmail.com>
2026-06-06 03:25:25 +0500

    feat: store photos as base64 in DB, remove file storage

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  const uploadsPath = join(__dirname, '..', 'uploads');
-  if (!fs.existsSync(uploadsPath)) {
-    fs.mkdirSync(uploadsPath, { recursive: true });
-  }
 
   app.enableCors({
     origin: [
